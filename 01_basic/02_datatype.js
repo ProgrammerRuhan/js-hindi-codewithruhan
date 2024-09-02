a342cb0412778c8506b2d970c84e28a6c5be1ff4
ProgrammerRuhan <143067310+ProgrammerRuhan@users.noreply.github.com>
2024-09-02 15:54:44 +0000

    conversion of data type

--- 01_basic/02_datatype.js
+++ 01_basic/02_datatype.js
@@ -1,3 +1,11 @@
 "use strict";
 
+let string = "ruhan";
+let number = 123456;
+let boolean = true/false;
+
+
+
+
 console.log('ruhan');
+
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let num = undefined;
+
+// console.log(typeof(num));
+
+let isNume = Number(num);
+
+// console.log(typeof isNume);
+// console.log(isNume);
+
+let inpt = 0;
+
+let resutl = Boolean(inpt);
+console.log(resutl);
