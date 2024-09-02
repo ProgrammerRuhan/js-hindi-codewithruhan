71c1fd67bff83a15d02f0828d3ca5da0513038cb
ProgrammerRuhan <143067310+ProgrammerRuhan@users.noreply.github.com>
2024-09-02 16:58:07 +0000

    data

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -10,4 +10,30 @@ let isNume = Number(num);
 let inpt = 0;
 
 let resutl = Boolean(inpt);
-console.log(resutl);
+//console.log(resutl);
+
+//      operation 
+
+// console.log("12" + 2 + 4); // it return 1224 because of first string
+// 
+// console.log(1 + 2 + "3"); // it return 33 first two number concat
+// 
+// console.log(1 + 3 + "4" + 3 + 3); // it return 4433 first concant only
+
+let x = 3;
+// console.log(x);
+
+let y = x++;
+// console.log(y, x);
+// console.log(x);
+
+
+console.log(null);
+console.log(typeof null);
+console.log(null <= 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null > 0);
+console.log(null < 0);
+
+
