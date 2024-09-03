75556a2c6f479644ef5c3fae013395dede3e2fe8
ProgrammerRuhan <143067310+ProgrammerRuhan@users.noreply.github.com>
2024-09-03 08:36:50 +0000

    update data

---
+++ 01_basic/04_dataType_summery.js
@@ -0,0 +1,68 @@
+// officially there are TWO type of data type 
+
+// first is premitive
+// string, number, boolean, bigInt, symbol, undefined, null,
+
+// secondly non premitive(refference)
+// Array, Object
+
+let arr = ['ruhan', "islam", "babu"];
+let obj = {
+    name : "ruhan",
+    age  : 23,
+    isMarried : false
+}
+
+// console.log(arr);
+// console.log(obj);
+// console.log(typeof obj);
+// console.log(typeof arr);
+
+// console.log(typeof strinn);
+
+
+let myName = "Ruhan";
+
+let anoName = myName;
+
+let user = {
+    name : "ruhan",
+    age : 20
+}
+
+user.age = 18
+
+// console.log(user);
+
+let user1 = 'ruhan islam';
+
+
+// console.log(user1[2]);
+
+
+let name1 =  `Ruhan`;
+let editName1 = name1.slice(1 , 4)
+
+// console.log(editName1, name1.length);
+
+let url =  `https:// ruhan .come%50/ru`;
+// console.log(url.replace(` `, `_`));
+
+
+let num1 = 1243;
+console.log(num1);
+
+let nnum1 = new Number(1222)
+console.log(nnum1);
+
+let arr11 = new Array("ruhan");
+console.log(arr11);
+
+let str11 = new String("my name")
+console.log(str11);
+
+let str12 = String("my second name")
+console.log(str12);
+
+
+
